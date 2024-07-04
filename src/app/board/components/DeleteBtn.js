dc59fe168626e1149860c31f07c223f10df6b1a2
jemicom <jemicom@gmail.com>
2024-07-04 15:56:07 +0900

    delete fetch

--- src/app/board/components/DeleteBtn.js
+++ src/app/board/components/DeleteBtn.js
@@ -4,19 +4,13 @@ const DeleteBtn = ({ board  }) => {
     const router = useRouter();
 
     const deleteHandle = (id) => {
-        fetch(`/api/board/${id}`, {
+        fetch(`https://next-08.vercel.app/api/board/${id}`, {
             method: 'DELETE',
-            // body: JSON.stringify( newItem ),
-            // headers: {
-            //   'Content-type': 'application/json; charset=UTF-8',
-            // },
         }).then(res=>res.json())
         .then(res=>{
             console.log('delete');
-            router.push('/board')
+            return router.push('/board')
         })
-        // .then(res=>redirect('/board'))
-        // client 사용 불가 
     };
   return (
     <button onClick={() => { deleteHandle(board.id) }}> delete </button>
